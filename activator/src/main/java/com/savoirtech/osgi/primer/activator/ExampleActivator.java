7489109cabba57b9dd70d76f553061296ab639fb
James Carman <jwcarman@gmail.com>
2017-04-10 22:40:42 -0400

    Using logging instead of system.out

--- activator/src/main/java/com/savoirtech/osgi/primer/activator/ExampleActivator.java
+++ activator/src/main/java/com/savoirtech/osgi/primer/activator/ExampleActivator.java
@@ -2,15 +2,27 @@ package com.savoirtech.osgi.primer.activator;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExampleActivator implements BundleActivator {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleActivator.class);
+
+//----------------------------------------------------------------------------------------------------------------------
+// BundleActivator Implementation
+//----------------------------------------------------------------------------------------------------------------------
+    
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        System.out.println("Starting my bundle...");
+        LOGGER.info("Starting my bundle...");
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-        System.out.println("Stopping my bundle...");
+        LOGGER.info("Stopping my bundle...");
     }
 }
